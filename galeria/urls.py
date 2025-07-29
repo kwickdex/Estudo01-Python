50dde42b2a831d0b34e47e321fe856c6a07d1786
Enzo Finotti <enzooliveirafinotti@gmail.com>
2025-07-29 10:53:41 -0300

    Até aula 05.02

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from galeria.views import index
-from galeria.views import imagem
+from galeria.views import index, imagem
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagens', imagem, name='imagem'),
+    path('imagem/<int:foto_id>', imagem, name='imagem'),
 ]
