7d4281ae4e494dc20755009cc3eba7cfbb841212
Enzo Finotti <enzooliveirafinotti@gmail.com>
2025-07-29 11:31:58 -0300

    Django: persistência de dados e Admin

--- galeria/views.py
+++ galeria/views.py
@@ -4,8 +4,19 @@ from galeria.models import Fotografia
 
 def index(request):
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publica = True);
-    return render(request, 'galeria/index.html', {"cards": fotografias})
+    return render(request, 'galeria/index.html', {'cards': fotografias})
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
-    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+def buscar(request):
+    
+    fotografia = Fotografia.objects.order_by("data_fotografia").filter(publica = True);
+    
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografia = fotografia.filter(nome__icontains=nome_a_buscar)
+    
+    return render(request, 'galeria/buscar.html', {'cards': fotografia})
